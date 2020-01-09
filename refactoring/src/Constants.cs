b455ef09bb986fb117392f54ffe2e91b3dc19066
Edoardo Lenzi <edoardolenzi9@gmail.com>
2020-01-09 18:36:48 +0100

    IKeyDeformatter, Constants
    * Interface introduction
    * code duplication
    * simplify choice, gateway factory

---
+++ refactoring/src/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Org.BouncyCastle.Crypto.Xml
+{
+    public enum RSAPadding
+    {
+        PKCS1,
+        OAEP
+
+    }
+
+    public class Constants
+    {
+        public static Dictionary<RSAPadding, string> RsaPaddingDictionary = new Dictionary<RSAPadding, string>()
+        {
+            {RSAPadding.OAEP, "RSA//OAEPPADDING"},
+            {RSAPadding.PKCS1, "RSA//PKCS1PADDING"}
+        };
+    }
+}
---
+++ refactoring/src/RSAFormatters/IKeyDeformatter.cs
@@ -0,0 +1,10 @@
+﻿using Org.BouncyCastle.Crypto.Parameters;
+
+namespace Org.BouncyCastle.Crypto.Xml.RSAKey
+{
+    interface IKeyDeformatter
+    {
+        byte[] DecryptKeyExchange(byte[] rgbData);
+        void SetKey(RsaKeyParameters key);
+    }
+}
